602266e76a9fbb6908f7238898a2937a17d399a6
zencashmoon <zencashmoon@outlook.com>
2018-06-23 18:30:50 +0200

    fix ipv6

--- ipv6-dns-workaround.js
+++ ipv6-dns-workaround.js
@@ -2,6 +2,6 @@
 
 const dns = require('dns'), {lookup} = dns;
 dns.lookup = function(name, opts, cb) {
-  if (typeof cb !== 'function') return lookup(name, {verbatim:true}, opts);
-  return lookup(name, Object.assign({verbatim:true}, opts), cb);
+  if (typeof cb !== 'function') return lookup(name, {verbatim:true,family:6}, opts);
+  return lookup(name, Object.assign({verbatim:true,family:6}, opts), cb);
 };
